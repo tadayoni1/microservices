95b6cbf4031d5f3385477086bfb310154ad1b58b
Pedram <tadayoni@gmail.com>
2019-10-18 20:14:08 -0700

    Initial commit

---
+++ src/test/java/net/tirgan/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tirgan.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
